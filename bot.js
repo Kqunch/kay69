f8b44ca5b7cc24f59958757f2e7ed3490150d312
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-08 16:35:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,10 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'kay') {
     	message.reply('https://gyazo.com/6c721595ca6a31dbbbd446f28ce5830d');
+          
+client.on('message', message => {
+    if (message.content === 'oldwaifu') {
+    	message.reply('https://gyazo.com/5ca4235d3eb29327daf33655473c0325');
   	}
 });
 
