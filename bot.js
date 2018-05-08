89b10cf8b76227f512063b313e991c0b94d81651
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-08 16:36:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'kay') {
-    	message.reply('https://gyazo.com/6c721595ca6a31dbbbd446f28ce5830d');
           
 client.on('message', message => {
     if (message.content === 'oldwaifu') {
