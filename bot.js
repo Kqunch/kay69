d5e7a9c86954ca5d4a561a42bebd03e82139ff6e
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-08 16:04:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'love') {
+    	message.reply('I love you :kissing_closed_eyes:');
   	}
 });
 
